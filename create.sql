8d9908b0047b336ee03313dc1961a98e1c6fc7a8
AristovaIrina <43518375+IrinaAristova@users.noreply.github.com>
2020-04-19 13:11:07 +0300

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Country(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR (50)
+);
+
+CREATE TABLE Athlete(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR (50),
+    id_country INT NOT NULL REFERENCES Country(id)
+);
+
+CREATE TABLE Sport(
+    id INT NOT NULL PRIMARY KEY,
+    category VARCHAR (50),
+    discipline VARCHAR (50),
+    events VARCHAR (50)
+);
+
+CREATE TABLE Competition(
+    id INT NOT NULL PRIMARY KEY,
+    year INT,
+    city VARCHAR (50)
+);
+
+CREATE TABLE Medal(
+    id INT NOT NULL PRIMARY KEY,
+    color VARCHAR (10),
+    id_athlete INT NOT NULL REFERENCES Athlete(id),
+    id_sport INT NOT NULL REFERENCES Sport(id),
+    id_competition INT NOT NULL REFERENCES Competition(id)
+);
+
